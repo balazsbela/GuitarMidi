accb595d76a6088c11bb3b19c7ac89de550521ce
Balazs Bela <balazsbela@gmail.com>
2015-01-18 14:18:35 +0000

    Initial commit

---
+++ JuceLibraryCode/modules/juce_audio_plugin_client/juce_audio_plugin_client.h
@@ -0,0 +1,5 @@
+// This is an auto-generated file to redirect any included
+// module headers to the correct external folder.
+
+#include "../../../../../../Downloads/juce-git/juce-git/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client.h"
+
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # GuitarMidi
+
 Experimenting with JUCE to create a VST plugin that generates MIDI out of guitar input.
+
+
+Currently not functional.
+
+
+Based on: https://github.com/Venetian/JuceSimpleMidiFileLoader
